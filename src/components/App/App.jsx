2e19566a62487ffea20da9f1182dd9aad8e0784f
aj-1904 <jainayush827@gmail.com>
2021-06-30 22:14:52 +0530

    Creating and Styling Nav Component

---
+++ src/components/App/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import Nav from "../Nav/Nav";
+
+function App() {
+  return (
+    <div className="app">
+      {/* Nav Section */}
+      <Nav />
+
+      {/* landing page */}
+
+      {/* Challenge Section */}
+
+      {/* Footer */}
+    </div>
+  );
+}
+
+export default App;
