42c3e310c349800a4b5e01625e07ad2b15ac00e1
aj-1904 <jainayush827@gmail.com>
2021-07-04 13:39:24 +0530

    Creating and Styling TestLetter Component

--- src/components/TypingChallenge/TypingChallenge.jsx
+++ src/components/TypingChallenge/TypingChallenge.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./TypingChallenge.css";
+import TestLetter from "../TestLetter/TestLetter";
 
 function TypingChallenge({
   selectedParagraph,
@@ -19,7 +20,14 @@ function TypingChallenge({
       </div>
       <div className="textarea-container">
         <div className="textarea-left">
-          <div className="textarea test-paragraph">{selectedParagraph}</div>
+          <div className="textarea test-paragraph">
+              {/* {selectedParagraph} */}
+              {
+                  testInfo.map((individualLetterInfo) => {
+                      return <TestLetter individualLetterInfo={individualLetterInfo}/>
+                  })
+              }
+              </div>
         </div>
         <div className="textarea-right">
           <textarea
